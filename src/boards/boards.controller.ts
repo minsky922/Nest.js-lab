e80ebaaf5d1c0e55a7feec032cf5d042517eac12
minsky922 <minsky922@gmail.com>
2024-04-07 23:44:57 +0900

    boards

---
+++ src/boards/boards.controller.ts
@@ -0,0 +1,40 @@
+import { Controller, Get, Post, Body, Param, Delete, Patch } from '@nestjs/common';
+import { Board, BoardStatus } from './board.model';
+import { BoardsService } from './boards.service';
+import { createBoardDto } from './dto/create-board.dto';
+
+@Controller('boards')
+export class BoardsController {
+    constructor(private boardsService: BoardsService) { }
+    
+    @Get('/')
+    getAllBoard(): Board[] {
+        return this.boardsService.getAllBoards();
+    }
+
+    @Post()
+    createBoard(
+        @Body() createBoardDto: createBoardDto 
+    ): Board {
+        return this.boardsService.createBoard(createBoardDto);
+    }
+    
+    @Get('/:id')
+    getBoardById(@Param('id') id: string): Board {
+        return this.boardsService.getBoardById(id)
+    }
+
+    @Delete('/:id')
+    deleteBoard(@Param('id') id: string): void {
+        this.boardsService.deleteBoard(id);
+    }
+
+    @Patch('/:id/status')
+    updateBoardStatus(
+        @Param('id') id: string,
+        @Body('status') status: BoardStatus,
+    ) {
+        return this.boardsService.updateBoardStatus(id, status)
+    }
+    
+}
