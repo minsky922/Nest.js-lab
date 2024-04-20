1d4ec7c02b9ce898bf5550ca820dacf809aba6f2
minsky922 <minsky922@gmail.com>
2024-04-20 23:20:01 +0900

    comment

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -8,13 +8,14 @@ import { Repository } from 'typeorm';
 import { User } from 'src/auth/user.entity';
 // import { v1 as uuid } from 'uuid';
 
+// repository pattern을 적용하지 않고 service에서 db로직 처리
 @Injectable()
 export class BoardsService {
 
     constructor(
         @InjectRepository(Board)
-        private boardRepository: Repository<Board> ,
-    ) {}
+        private boardRepository: Repository<Board>,
+    ) { }
     // private boards: Board[] = [];
 
     // getAllBoards(): Board[] {
@@ -23,7 +24,7 @@ export class BoardsService {
 
     async getAllBoards(
         user: User
-    ): Promise <Board[]> {
+    ): Promise<Board[]> {
         const query = this.boardRepository.createQueryBuilder('board');
 
         query.where('board.userId = :userId', { userId: user.id });
@@ -48,7 +49,7 @@ export class BoardsService {
     // }
 
     async createBoard(createBoardDto: createBoardDto, user: User): Promise<Board> {
-        const {title, description} = createBoardDto;
+        const { title, description } = createBoardDto;
 
         const board = this.boardRepository.create({
             title,
@@ -61,10 +62,10 @@ export class BoardsService {
         return board;
     }
 
-    async getBoardById(id: number): Promise <Board> {
+    async getBoardById(id: number): Promise<Board> {
         const found = await this.boardRepository.findOne({ where: { id } });
 
-        if(!found) {
+        if (!found) {
             throw new NotFoundException(`Can't find Board with id ${id}`);
         }
         return found;
@@ -82,14 +83,14 @@ export class BoardsService {
     // }
 
     async deleteBoard(id: number, user: User): Promise<void> {
-        const result = await this.boardRepository.delete({id, user});
+        const result = await this.boardRepository.delete({ id, user });
 
-        if(result.affected === 0){
+        if (result.affected === 0) {
             throw new NotFoundException(`Can't find Board with id ${id}`)
         }
         console.log('result', result);
     }
-    
+
     // deleteBoard(id: string): void {
     //     const found = this.getBoardById(id);
     //     this.boards = this.boards.filter((board) => board.id !== id);
